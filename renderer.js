593a0fb918c017a803b302883bd16b89c26a39d2
ustczwq <ziuowq@gmail.com>
2018-12-17 19:10:55 +0800

    add selecting and setting  serial com ports

--- renderer.js
+++ renderer.js
@@ -1,8 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var serialport = require('serialport');
-
-serialport.list(function(err, ports) {
+window.$ = window.jQuery = require('jquery')
+let serialport = require('serialport');
+let port = null;
+serialport.list((err, ports) => {
+    for (let item of ports) {
+        $('.com').append(`<option>${item.comName}</option>`);
+    }
     console.log(ports);
 });
