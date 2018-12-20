7674e3ddda3b58dbc9f3409f8ddd8cf46842e7db
ustczwq <ziuowq@gmail.com>
2018-12-20 12:34:09 +0800

    can reload com ports using 'mousedown'

--- renderer.js
+++ renderer.js
@@ -1,12 +1,27 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+// window.$ = window.jQuery = require('jquery')
+// let serialport = require('serialport');
+// let port = null;
+// serialport.list((err, ports) => {
+//     for (let item of ports) {
+//         $('.com').append(`<option>${item.comName}</option>`);
+//     }
+//     console.log(ports);
+// });
+
 window.$ = window.jQuery = require('jquery')
 let serialport = require('serialport');
 let port = null;
-serialport.list((err, ports) => {
-    for (let item of ports) {
-        $('.com').append(`<option>${item.comName}</option>`);
-    }
-    console.log(ports);
+
+$("#Port").on("mousedown", function(){
+    //$(this).empty();
+    serialport.list((err, ports) => {
+        $(this).empty();
+        for (let item of ports) {
+            console.log(item.comName);
+            $(this).append(`<option>${item.comName}</option>`);
+        }
+    })
 });
